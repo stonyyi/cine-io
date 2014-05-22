08ae59a37bf4d0d576b50e8ddcfba8cffdc04be0
Thomas Shafer <thomasjshafer@gmail.com>
2014-05-22 14:23:08 -0700

    added api key and api secret to kensa

--- development/heroku_test_provision.rb
+++ development/heroku_test_provision.rb
@@ -1,7 +1,10 @@
-api_key = ENV['CINE_IO_API_KEY']
+api_values = {
+  api_key: ENV['CINE_IO_API_KEY'],
+  api_secret: ENV['CINE_IO_API_SECRET']
+}
 
-if api_key != ''
-  puts "I got cine.io api key #{api_key}"
+if api_values.fetch(:api_key) != '' && api_values.fetch(:api_secret) != ''
+  puts "I got cine.io api key and secret: #{api_values.inspect}"
 else
-  abort "Failed to get api key"
+  abort "Failed to get api key #{api_values.inspect}"
 end
