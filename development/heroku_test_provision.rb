1b28f5d8bcda8d000f8d5a8a6685ad387a3ad4c9
Thomas Shafer <thomasjshafer@gmail.com>
2014-05-30 10:50:15 -0700

    fixed heroku_test_provision to use new keys

--- development/heroku_test_provision.rb
+++ development/heroku_test_provision.rb
@@ -1,10 +1,10 @@
 # kensa run ruby development/heroku_test_provision.rb
 api_values = {
-  api_key: ENV['CINE_IO_API_KEY'],
-  api_secret: ENV['CINE_IO_API_SECRET']
+  public_key: ENV['CINE_IO_PUBLIC_KEY'],
+  secret_key: ENV['CINE_IO_SECRET_KEY']
 }
 
-if api_values.fetch(:api_key) != '' && api_values.fetch(:api_secret) != ''
+if api_values.fetch(:public_key) && api_values.fetch(:secret_key)
   puts "I got cine.io public key and secret: #{api_values.inspect}"
 else
   abort "Failed to get public key #{api_values.inspect}"
