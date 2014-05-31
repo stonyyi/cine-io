f3a3a2bf3d21cba709a9f55d88156f37877da9a1
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-05-30 17:15:54 -0700

    blog link

--- apps/main/app/components/layout/footer.jsx
+++ apps/main/app/components/layout/footer.jsx
@@ -11,9 +11,10 @@ module.exports = React.createClass({
         </div>
         <div className="row">
           <ul className="legal-links">
+            <li><a href="https://blog.cine.io/">Blog</a></li>
+            <li><a href="https://cineio.uservoice.com/">Feedback / Support</a></li>
             <li><a href="/legal/terms-of-service">Terms of Service</a></li>
             <li><a href="/legal/privacy-policy">Privacy Policy</a></li>
-            <li><a href="https://cineio.uservoice.com/">Feedback / Support</a></li>
           </ul>
         </div>
       </footer>
