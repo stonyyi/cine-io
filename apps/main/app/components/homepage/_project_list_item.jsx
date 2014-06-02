e428d4d5266535fd56224a282224bfe32fda1e10
Thomas Shafer <thomasjshafer@gmail.com>
2014-06-02 12:30:52 -0700

    added some "settings"

--- apps/main/app/components/homepage/_project_list_item.jsx
+++ apps/main/app/components/homepage/_project_list_item.jsx
@@ -9,30 +9,46 @@ module.exports = React.createClass({
   propTypes: {
     model: React.PropTypes.instanceOf(Project).isRequired
   },
+  getInitialState: function(){
+    return {showingSettings: false};
+  },
   getBackboneObjects: function(){
     return this.props.model;
   },
   accessSettings: function (e) {
     e.preventDefault();
     e.stopPropagation();
-    alert('Settings not implemented');
+    this.setState({showingSettings: !this.state.showingSettings});
   },
   showStreams: function(e){
     this._owner.selectProject(this.props.model);
   },
-  toggleSecretKey: function (e) {
-    e.preventDefault();
-    this.setState({showApiKey: !this.state.showApiKey});
-  },
   render: function() {
-    var model = this.props.model
-      , classes = cx({selected: this.props.selected})
-    ;
+    var model = this.props.model,
+      classes = cx({selected: this.props.selected}),
+      settings = '';
 
+    if (this.state.showingSettings){
+      settings = (
+        <div>
+          <dl>
+            <dt>Public key</dt>
+            <dd>{model.get('publicKey')}</dd>
+            <dt>Secret key</dt>
+            <dd>{model.get('secretKey')}</dd>
+            <dt>Streams count</dt>
+            <dd>{model.get('streamsCount')}</dd>
+          </dl>
+        </div>
+      );
+    }
     return (
       <tr onClick={this.showStreams} className={classes}>
-        <td> {model.get('name')} </td>
-        <td>
+        <td className='no-move'>
+          <div>{model.get('name')}</div>
+          {settings}
+        </td>
+        <td className='place-top'>
           <a href='' onClick={this.accessSettings}>
             <i className="fa fa-cogs fa-2x"></i>
           </a>
