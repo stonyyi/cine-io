e36ea11c8bb6a86bc602eb804c6f574bac321a87
Thomas Shafer <thomasjshafer@gmail.com>
2014-08-04 14:22:01 -0700

    added label at bottom

--- apps/main/app/components/usage_reports/show.jsx
+++ apps/main/app/components/usage_reports/show.jsx
@@ -10,7 +10,7 @@ _ = require('underscore');
 
 var UsageGraph = React.createClass({
   displayName: "UsageGraph",
-  mixins: [Cine.lib('backbone_mixin')],
+  mixins: [Cine.lib('requires_app'), Cine.lib('backbone_mixin')],
   getInitialState: function(){
     return {chartsReady: false};
   },
@@ -27,13 +27,14 @@ var UsageGraph = React.createClass({
     var ltm = UsageReport.lastThreeMonths(),
       model = this.props.model,
       data = [["Month", "Usage", "Cap"]],
-      planUsageInBytes = UsageReport.maxUsagePerAccount(App.currentUser),
+      planUsageInBytes = UsageReport.maxUsagePerAccount(this.props.app.currentUser),
       formatString = humanizeBytes.formatString(planUsageInBytes),
       planUsage = planUsageInBytes / humanizeBytes[formatString];
 
     _.each(ltm, function(month){
       var monthlyUsage = model.get(month.format) / humanizeBytes[formatString];
-      data.push([month.format, monthlyUsage, planUsage]);
+      bottomLabel = month.format + " ("+humanizeBytes(model.get(month.format))+")"
+      data.push([bottomLabel, monthlyUsage, planUsage]);
     });
 
     var options = {
@@ -73,7 +74,7 @@ module.exports = React.createClass({
           <div className='row'>
             <div className='small-12 columns'>
               <h1>Usage Report</h1>
-              <UsageGraph model={this.props.model} />
+              <UsageGraph model={this.props.model} app={this.props.app}/>
             </div>
           </div>
         </div>
