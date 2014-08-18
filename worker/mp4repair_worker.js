bd0a70249ab254db01ad475a15f14850146271ac
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-08-18 11:15:31 -0700

    removing test workers

--- worker/mp4repair_worker.js
+++
@@ -1,7 +0,0 @@
-require('coffee-script/register')
-Cine = require('./config/cine_server')
-repairFile = Cine.server_lib("stream_recordings/transcode_recording")
-
-repairFile('wrongaspect.mp4', 'rightaspect.mp4', function() {
-  console.log("DONE!")
-})
