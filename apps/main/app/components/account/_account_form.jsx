03cac5ba5701d748f2cecc9aec2ef5b9cd52b9ca
Thomas Shafer <thomasjshafer@gmail.com>
2014-08-19 17:30:43 -0700

    use tempPlan on account instead of plan on user

--- apps/main/app/components/account/_account_form.jsx
+++ apps/main/app/components/account/_account_form.jsx
@@ -1,8 +1,6 @@
 /** @jsx React.DOM */
 var React = require('react'),
   User = Cine.model('user'),
-  _ = require('underscore'),
-  capitalize = Cine.lib('capitalize'),
   authentication = Cine.lib('authentication');
 
 module.exports = React.createClass({
@@ -10,7 +8,7 @@ module.exports = React.createClass({
   mixins: [Cine.lib('requires_app')],
   getInitialState: function(){
     var currentUser = this.props.app.currentUser;
-    return {name: currentUser.get('name'), email: currentUser.get('email'), plan: currentUser.get('plan'), initialPlan: currentUser.get('plan')};
+    return {name: currentUser.get('name'), email: currentUser.get('email')};
   },
   changeName: function(event) {
     this.setState({name: event.target.value});
@@ -18,26 +16,14 @@ module.exports = React.createClass({
   changeEmail: function(event) {
     this.setState({email: event.target.value});
   },
-  changePlan: function(event) {
-    this.setState({plan: event.target.value});
-  },
-  updateAccount: function(event){
+  updateUser: function(event){
     event.preventDefault();
     var form = jQuery(event.currentTarget);
-    authentication.updateAccount(this.props.app, form, {success: this.updateSuccess});
-  },
-  updateSuccess: function(){
-    if (this.state.plan != this.state.initialPlan){
-      this.props.app.tracker.planChange(this.state.plan);
-      this.setState({initialPlan: this.state.plan});
-    }
+    authentication.updateAccount(this.props.app, form);
   },
   render: function() {
-    var planOptions = _.map(User.plans, function(plan) {
-      return (<option key={plan} value={plan}>{capitalize(plan)}</option>);
-    });
     return (
-      <form onSubmit={this.updateAccount}>
+      <form onSubmit={this.updateUser}>
         <div className="row">
           <div className="large-12 columns">
             <label>Name
@@ -49,15 +35,6 @@ module.exports = React.createClass({
           <div className="large-12 columns">
             <label>Email
               <input type="text" placeholder="Your email" value={this.state.email} onChange={this.changeEmail} name='email'/>
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="large-12 columns">
-            <label>Plan
-              <select value={this.state.plan} onChange={this.changePlan} name='plan'>
-                {planOptions}
-              </select>
             </label>
           </div>
         </div>
--- apps/main/app/components/billing/show.jsx
+++ apps/main/app/components/billing/show.jsx
@@ -1,7 +1,11 @@
 /** @jsx React.DOM */
 var React = require('react'),
 PageWrapper = Cine.component('layout/_page_wrapper'),
-NewCreditCard = Cine.component('billing/_new_credit_card');
+NewCreditCard = Cine.component('billing/_new_credit_card'),
+  User = Cine.model('user'),
+  _ = require('underscore'),
+  capitalize = Cine.lib('capitalize'),
+
 CurrentCreditCard = Cine.component('billing/_current_credit_card');
 
 module.exports = React.createClass({
@@ -10,11 +14,60 @@ module.exports = React.createClass({
   getBackboneObjects: function(){
     return this.props.app.currentAccount();
   },
+  getInitialState: function(){
+    var currentAccount = this.props.app.currentAccount();
+    return {tempPlan: currentAccount.get('tempPlan'), initialTempPlan: currentAccount.get('tempPlan')};
+  },
+  changePlan: function(event) {
+    this.setState({tempPlan: event.target.value});
+  },
+  updateSuccess: function(){
+    if (this.state.plan != this.state.initialPlan){
+      this.props.app.tracker.planChange(this.state.plan);
+      this.setState({initialPlan: this.state.plan});
+    }
+  },
+  updateAccount: function(e){
+    e.preventDefault();
+    var self = this;
+      ca = self.props.app.currentAccount();
+    ca.set({tempPlan: this.state.tempPlan})
+    ca.save(null,{
+      success: function(model, response, options){
+        self.updateSuccess()
+        model.store();
+        self.props.app.flash('Successfully updated plan.', 'success');
+      },
+      error: function(model, response, options){
+      }
+    });
+  },
   render: function() {
+    var planOptions = _.map(User.plans, function(plan) {
+      return (<option key={plan} value={plan}>{capitalize(plan)}</option>);
+    });
+
     var CardModule = this.props.app.currentAccount().get('stripeCard') ? CurrentCreditCard : NewCreditCard;
     return (
       <PageWrapper app={this.props.app}>
         <h1 className="bottom-margin-1">Billing Information</h1>
+        <form onSubmit={this.updateAccount}>
+          <div className="row">
+            <div className="large-12 columns">
+              <label>Plan
+                <select value={this.state.tempPlan} onChange={this.changePlan} name='plan'>
+                  {planOptions}
+                </select>
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="large-12 columns">
+              <button type='submit'>Save</button>
+            </div>
+          </div>
+        </form>
+
         <CardModule app={this.props.app} />
       </PageWrapper>
     );
