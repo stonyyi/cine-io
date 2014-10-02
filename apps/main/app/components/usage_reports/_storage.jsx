ad8c500dd0951433d713d575e52b6310e0ec5e64
Thomas Shafer <thomasjshafer@gmail.com>
2014-10-02 14:28:25 -0700

    show a bar chart when the storage is over. it should really be a combo chart to have a threshold I should also change the bandwidth to use a combo chart for threshold too.

--- apps/main/app/components/usage_reports/_storage.jsx
+++ apps/main/app/components/usage_reports/_storage.jsx
@@ -18,17 +18,14 @@ module.exports = React.createClass({
       this.loadChart();
     }
   },
-  loadChart: function(){
+  drawPieChart: function(usedStorageInBytes, planStorageInBytes){
     var
-      storageUsage = this.props.model.get('storage');
-      planStorageInBytes = UsageReport.maxUsagePerAccount(this.props.app.currentAccount(), 'storage'),
-      availableStorage = planStorageInBytes - storageUsage;
-
-    var data = google.visualization.arrayToDataTable([
-      ['Storage', 'Used'],
-      ["Used (" + humanizeBytes(storageUsage)+")",     storageUsage],
-      ["Free ("+ humanizeBytes(availableStorage)+")", availableStorage]
-    ]);
+      availableStorageInBytes = planStorageInBytes - usedStorageInBytes,
+      data = google.visualization.arrayToDataTable([
+        ['Storage', 'Used'],
+        ["Used (" + humanizeBytes(usedStorageInBytes)+")",     usedStorageInBytes],
+        ["Free ("+ humanizeBytes(availableStorageInBytes)+")", availableStorageInBytes]
+      ]);
 
     var options = {
       title: 'Cloud Storage',
@@ -38,6 +35,37 @@ module.exports = React.createClass({
 
     var chart = new google.visualization.PieChart(this.refs['chartDiv'].getDOMNode());
     chart.draw(data, options);
+  },
+  drawBarChart: function(usedStorageInBytes, planStorageInBytes){
+    var
+      formatString = humanizeBytes.formatString(planStorageInBytes),
+      data = [["Total Storage", { role: 'annotation' }, "Usage"]],
+      storageAvailableInFormat = planStorageInBytes / humanizeBytes[formatString]
+      storageUsageInFormat = usedStorageInBytes / humanizeBytes[formatString]
+      dataOptions = ["", humanizeBytes(usedStorageInBytes), storageUsageInFormat];
+      data.push(dataOptions)
+      // data.push();
+
+    var options = {
+      vAxis: {title: "Storage ("+formatString+")", minValue: 0},
+      hAxis: {title: "Used Storage"},
+      seriesType: "bars",
+      enableInteractivity: false,
+      legend: {position: 'none'}
+    };
+
+    var chart = new google.visualization.ComboChart(this.refs['chartDiv'].getDOMNode());
+    chart.draw(google.visualization.arrayToDataTable(data), options);
+  },
+  loadChart: function(){
+    var
+      usedStorageInBytes = this.props.model.get('storage');
+      planStorageInBytes = UsageReport.maxUsagePerAccount(this.props.app.currentAccount(), 'storage');
+    if (usedStorageInBytes <= planStorageInBytes) {
+      this.drawPieChart(usedStorageInBytes, planStorageInBytes);
+    }else{
+      this.drawBarChart(usedStorageInBytes, planStorageInBytes);
+    }
   },
   render: function(){
     return (<div ref="chartDiv" />);
