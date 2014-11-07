6ecce8c1ae35fb7891280f16fd20a2153c980842
Thomas Shafer <thomasjshafer@gmail.com>
2014-11-06 17:34:38 -0800

    added submitting state to create stream button

--- apps/main/app/components/homepage/_new_stream_button.jsx
+++ apps/main/app/components/homepage/_new_stream_button.jsx
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 var
   React = require('react'),
+  cx = Cine.lib('cx'),
   Project = Cine.model('project'),
   Stream = Cine.model('stream');
 
@@ -13,10 +14,13 @@ module.exports = React.createClass({
   getInitialState: function(){
     return {submitting: false};
   },
+  doNothing: function(e){
+    e.preventDefault();
+  },
   createNewStream: function(e){
     e.preventDefault();
     if(this.state.submitting){return;}
-    this.setState({submitting: true})
+    this.setState({submitting: true});
     var self = this,
       p = new Stream({secretKey: this.props.model.get('secretKey')}, {app: this.props.app});
     p.save(null, {
@@ -37,8 +41,12 @@ module.exports = React.createClass({
     });
   },
   render: function(){
+    var
+      disabled = this.state.submitting,
+      linkClasses = cx({right: true, disabled: disabled}),
+      clickHandler = disabled ? this.doNothing : this.createNewStream;
     return (
-      <a href='' className='right' onClick={this.createNewStream}>
+      <a href='' className={linkClasses} disabled={disabled} onClick={clickHandler}>
         <span className="fa-stack fa-lg">
           <i className="fa fa-square fa-stack-2x"></i>
           <i className="fa fa-plus fa-stack-1x fa-inverse"></i>
