1941e187758775c4cc77ca9ed8e659e8910d80b6
Thomas Shafer <thomasjshafer@gmail.com>
2014-11-06 17:28:47 -0800

    made CurrentCreditCard jsx have a nice disabled state

--- apps/main/app/components/account/_current_credit_card.jsx
+++ apps/main/app/components/account/_current_credit_card.jsx
@@ -1,12 +1,23 @@
 /** @jsx React.DOM */
 var React = require('react');
 
+
 module.exports = React.createClass({
   displayName: 'CurrentCreditCard',
   mixins: [Cine.lib('requires_app')],
+  getInitialState: function(){
+    return {isDeleting: false};
+  },
+  doNothing: function(e){
+    e.preventDefault();
+  },
   deleteCard: function(event){
-    var app = this.props.app;
     event.preventDefault();
+    if(this.state.isDeleting){return;}
+    this.setState({isDeleting: true});
+    var
+      self = this,
+      app = this.props.app;
     app.currentAccount().set('deleteCard', this.props.app.currentAccount().get('stripeCard').id);
     app.currentAccount().save(null, {
       success: function(model, response, options){
@@ -21,20 +32,32 @@ module.exports = React.createClass({
         }
         model.store();
         app.flash('Successfully removed credit card.', 'success');
+        if (self.isMounted()){
+          self.setState({isDeleting: false});
+        }
       },
       error: function(model, response, options){
         model.unset('deleteCard');
         app.flash('Could not remove credit card.', 'alert');
+        if (self.isMounted()){
+          self.setState({isDeleting: false});
+        }
       }
     });
   },
   render: function() {
     var cu = this.props.app.currentAccount(),
-      card = cu.get('stripeCard');
+      card = cu.get('stripeCard'),
+      cardAction;
+    if(this.state.isDeleting){
+      cardAction = (<a href="" className="disabled" disabled onClick={this.doNothing}><span className="fa fa-times" /></a>);
+    }else{
+      cardAction = (<a href="" onClick={this.deleteCard}><span className="fa fa-times" /></a>);
+    }
     return (
       <div>
         Card on file: {card.brand}, {card.last4}, {card.exp_month} / {card.exp_year}
-        <a href="" onClick={this.deleteCard}><span className="fa fa-times" /></a>
+        {cardAction}
       </div>
     );
   }
