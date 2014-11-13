a189aa27791960d3a51974317a2822b9257096da
Thomas Shafer <thomasjshafer@gmail.com>
2014-11-13 13:10:02 -0800

    closer to the final image pixel pushing

--- apps/main/app/components/homepage/_home_hero.jsx
+++ apps/main/app/components/homepage/_home_hero.jsx
@@ -6,19 +6,6 @@ var
 module.exports = React.createClass({
   displayName: 'HomeHero',
   mixins: [],
-  getInitialState: function(){
-    return {rotateVideo: true, showPlayers: true};
-  },
-  componentDidMount: function () {
-    // var showPlayersTimeout = setTimeout(this.showPlayers, 3200);
-    // this.setState({rotateVideo: true, showPlayersTimeout: showPlayersTimeout});
-  },
-  componentWillUnmount: function () {
-    clearTimeout(this.state.showPlayersTimeout)
-  },
-  showPlayers: function(){
-    this.setState({showPlayers: true});
-  },
   render: function() {
     // <div className="hero-image broadcast"><img src="/images/home-hero/cine-broadcast.png" /></div>
     // <div className="hero-image arrow"><img src="/images/home-hero/cine-arrow.png" /></div>
@@ -26,50 +13,46 @@ module.exports = React.createClass({
     // <div className="hero-image arrow"><img src="/images/home-hero/cine-arrow.png" /></div>
     // <div className="hero-image play"><img src="/images/home-hero/cine-play.png" /></div>
 
-
-    var stageClassName = cx({rotate: this.state.rotateVideo});
-    playersClassnameOptions = {players: true, invisible: !this.state.showPlayers};
-    var playersClassname = cx(playersClassnameOptions);
+    var rings = (
+      <div className="hero-item rings">
+        <div className="ring"></div>
+        <div className="ring"></div>
+        <div className="ring"></div>
+        <div className="ring"></div>
+      </div>
+    );
     return (
       <div className='home-hero'>
         <div className="row">
 
-          <div className="nothing">
+          <div>
             <div className="hero-row hero-flow">
-              <div className="hero-item">
-                <div className='table-row broadcaster-wrapper'>
+              <div className="hero-item broadcaster-wrapper">
+                <div className='table-row'>
                   <div className="hero-item broadcaster">
                     <div className="hero-image broadcast">
                       <img src="/images/home-hero/cine-broadcast.png" />
                     </div>
                   </div>
                   <div className="hero-item stage">
-                    <video className={stageClassName} muted={true} autoPlay={true} loop={true}>
+                    <video muted={true} autoPlay={true} loop={true}>
                       <source src="http://vod.cine.io/homepage/fireworks_320.webm" type="video/webm" />
                       <source src="http://vod.cine.io/homepage/fireworks_320.mp4" type="video/mp4" />
                     </video>
                   </div>
                 </div>
               </div>
-              <div className="hero-item arrow">
-                <div className="hero-image">
-                  <img src="/images/home-hero/cine-arrow.png" />
-                </div>
-              </div>
+              {rings}
               <div className="hero-item cloud">
                 <div className="hero-image">
                   <img src="/images/home-hero/cine-cloud.png" />
                 </div>
               </div>
 
-              <div className="hero-item arrow">
-                <div className="hero-image">
-                  <img src="/images/home-hero/cine-arrow.png" />
-                </div>
-              </div>
+              {rings}
 
               <div className="hero-item watch">
-                <div className={playersClassname}>
+                <div className="players">
                   <div className="hero-image laptop-video">
                     <video muted={true} autoPlay={true} loop={true}>
                       <source src="http://vod.cine.io/homepage/fireworks_320.webm" type="video/webm" />
@@ -95,18 +78,26 @@ module.exports = React.createClass({
               </div>
             </div>
           </div>
-          <div className='hero-row hero-text'>
-            <div className="hero-item">
-              <h3>Broadcast</h3>
-              <p>Broadcast using our native SDKs or popular broadcast software.</p>
-            </div>
-            <div className='hero-item'>
-              <h3>Transcode, Record, Distribute</h3>
-              <p>Automatic transcoding and transmuxing for playback on any device. Record directly to the cloud.</p>
-            </div>
-            <div className='hero-item'>
-              <h3>Watch</h3>
-              <p>View your content live or at a later time on any device.</p>
+          <div className="hero-text-wrapper">
+            <div className='hero-row hero-text'>
+              <div className="hero-item broadcaster-wrapper">
+                <div>
+                  <h3>Broadcast</h3>
+                  <p>Broadcast using our native SDKs or popular broadcast software.</p>
+                </div>
+              </div>
+              <div className='hero-item cloud'>
+                <div>
+                  <h3>Transcode, Record, Distribute</h3>
+                  <p>Automatic transcoding and transmuxing for playback on any device. Record directly to the cloud.</p>
+                </div>
+              </div>
+              <div className='hero-item players'>
+                <div>
+                  <h3>Watch</h3>
+                  <p>View your content live or at a later time on any device.</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
