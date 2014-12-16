95cd5bce28e48ad503e6c2c10afe566f57fbc7f7
Thomas Shafer <thomasjshafer@gmail.com>
2014-12-16 13:35:59 -0800

    redirect to dashboard on login of pricing page

--- apps/home/main/app/components/homepage/pricing.jsx
+++ apps/home/main/app/components/homepage/pricing.jsx
@@ -6,7 +6,15 @@ PageWrapper = Cine.component('layout/_page_wrapper');
 module.exports = React.createClass({
   displayName: 'HomepagePricing',
   mixins: [Cine.lib('requires_app')],
-
+  componentDidMount: function() {
+    this.props.app.currentUser.on('login', this.redirectToDashboard);
+  },
+  componentWillUnmount: function() {
+    this.props.app.currentUser.off('login', this.redirectToDashboard);
+  },
+  redirectToDashboard: function(){
+    this.props.app.router.redirectTo('/dashboard');
+  },
   render: function() {
 
     return (
